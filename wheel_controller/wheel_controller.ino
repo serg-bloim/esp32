9e5ce529c85be2f66370e32f1e7cbf349b70c84a
serg.bloim <serg.bloim@gmail.com>
2023-10-19 16:35:02 -0400

    project for a wheel controller

---
+++ wheel_controller/wheel_controller.ino
@@ -0,0 +1,44 @@
+#if ARDUINO_USB_MODE
+#warning This sketch should be used when USB is in OTG mode
+err
+void setup(){}
+void loop(){}
+#else
+
+#include "USB.h"
+#include "USBHIDKeyboard.h"
+USBHIDKeyboard Keyboard;
+
+const int buttonPin = 0;          // input pin for pushbutton
+int previousButtonState = HIGH;   // for checking the state of a pushButton
+int counter = 0;                  // button push counter
+
+void setup() {
+  // make the pushButton pin an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+  USB.begin();
+  Serial.setPins(37,39);
+  Serial.begin(9600);
+  Serial.println("Started");
+}
+void loop() {
+  // read the pushbutton:
+  int buttonState = digitalRead(buttonPin);
+  // if the button state has changed,
+  if ((buttonState != previousButtonState)
+      // and it's currently pressed:
+      && (buttonState == LOW)) {
+    // increment the button counter
+    counter++;
+    // type out a message
+    auto msg = "You pressed the button " + String(counter) + " times.";
+    Serial.println(msg);
+
+    Keyboard.println(msg);
+  }
+  // save the current button state for comparison next time:
+  previousButtonState = buttonState;
+}
+#endif /* ARDUINO_USB_MODE */
