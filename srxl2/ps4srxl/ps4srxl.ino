cf9575cb1b277718e2b166054ad7e148c9f31077
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2024-07-22 21:34:47 -0400

    upd. fixed ps4 connection

--- srxl2/ps4srxl/ps4srxl.ino
+++ srxl2/ps4srxl/ps4srxl.ino
@@ -1,33 +1,37 @@
 #include "StaticConfig.h"
-#include "RemoteReceiver.hpp"
-#include <SoftwareSerial.h>
+// #include "RemoteReceiver.hpp"
+// #include <SoftwareSerial.h>
+#include "utils.hpp"
 #include "Blinker.hpp"
 #include "Ps4Client.hpp"
 
-SoftwareSerial serial;
-RemoteReceiver srxl(serial, DEVICES_REMOTE_RECEIVER);
+// SoftwareSerial serial;
+// RemoteReceiver srxl(serial, DEVICES_REMOTE_RECEIVER);
 Blinker blk(2, 500);
 Ps4Client ps4;
 
 void setup() {
-  blk.begin();
-  srxl.begin(SRXL_PIN);
   Serial.begin(115200);
-  ps4.begin("a8:2b:b9:45:ac:89");
   delay(2000);
+  Serial.println("setup 1");
+  blk.begin();
+  // srxl.begin(SRXL_PIN);
+  ps4.begin("a8:2b:b9:45:ac:89");
   Serial.println("Start");
 }
 
 void loop() {
+  Serial.println("loop");
   blk.update();
-  srxl.set_channel(0, convert_unsigned(ps4.throttle));
-  srxl.set_channel(1, convert_signed(ps4.pitch));
-  srxl.set_channel(2, convert_signed(ps4.roll));
-  srxl.set_channel(3, convert_signed(ps4.yaw));
-  // srxl.set_channel(4, ps4.safe_mode);
-  // srxl.set_channel(5, ps4.panic_mode);
-  // srxl.set_channel(6, ps4.ch7);
-  srxl.update();
+  // srxl.set_channel(0, convert_unsigned(ps4.throttle));
+  // srxl.set_channel(1, convert_signed(ps4.pitch));
+  // srxl.set_channel(2, convert_signed(ps4.roll));
+  // srxl.set_channel(3, convert_signed(ps4.yaw));
+  // // srxl.set_channel(4, ps4.safe_mode);
+  // // srxl.set_channel(5, ps4.panic_mode);
+  // // srxl.set_channel(6, ps4.ch7);
+  // srxl.update();
+  delay(1000);
 }
 
 void on_ps4_event(){
