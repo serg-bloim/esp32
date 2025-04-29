b470de167665f549b417991ce02ce8c780fbc334
serg.bloim <serg.bloim@gmail.com>
2025-04-29 17:23:02 -0400

    sends request to backend

--- hamster/hamster.ino
+++ hamster/hamster.ino
@@ -1,16 +1,47 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ASUS";
+const char* password = "A4388Ed8843";
+const char* api_key = "7AKI7ZVF3RPEIS2Y";
+
 const int pin = 34;
 const int treshold = 2048;
-bool state = true;
-bool has_state_changed = true;
+bool state = false;
+bool has_state_changed = false;
+int rotations = 0;
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected");
 }
 
 void loop() {
   processState();
   if(has_state_changed){
     Serial.printf("State: %d \n", state);
+    if(state == 0){
+      // The signal has just been HIGH and wend back LOW
+      rotations++;
+      Serial.printf("Rotations: %d \n", rotations);
+      if(WiFi.status() == WL_CONNECTED){
+        HTTPClient http;
+        String url = String("https://api.thingspeak.com/update?api_key=") + api_key + "&field2=" + String(rotations);
+        http.begin(url);
+        int status_code = http.GET();
+        Serial.printf("Backend response: %d\n", status_code);
+        http.end();
+      }else{
+        Serial.println("WiFi not connected");
+      }
+    }
   }
   delay(50);
 }
