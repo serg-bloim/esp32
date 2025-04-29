4c0ca0c83358a5699dd7474da3f42335b2e31281
serg.bloim <serg.bloim@gmail.com>
2025-04-29 16:23:50 -0400

    upd

--- hamster/hamster.ino
+++ hamster/hamster.ino
@@ -1,11 +1,26 @@
 const int pin = 34;
+const int treshold = 2048;
+bool state = true;
+bool has_state_changed = true;
 void setup() {
   Serial.begin(9600);
   delay(1000);
 }
 
 void loop() {
+  processState();
+  if(has_state_changed){
+    Serial.printf("State: %d \n", state);
+  }
+  delay(50);
+}
+
+bool processState(){
   int adcVal = analogRead(pin);
-  Serial.printf("Value: %d \n", adcVal);
-  delay(500);
+  // int adcVal = 100;
+  bool new_state = adcVal > treshold;
+  // bool new_state = true;
+  has_state_changed = new_state != state;
+  state = new_state;
+  return has_state_changed;
 }
